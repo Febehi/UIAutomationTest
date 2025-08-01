7e7c5af23242885f6fbe7d7d101ff6ff84fe6314
Febehi <febehi@integrationobjects.com>
2025-08-01 11:17:59 +0100

    Update LogoutPageLocators.py

--- resources/LogoutPageLocators.py
+++ resources/LogoutPageLocators.py
@@ -2,6 +2,6 @@ from selenium.webdriver.common.by import By
 
 class LogoutPageLocators:
 
-    IMAGE_AVATAR=(By.XPATH,"//*[@id='app']/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/img")
-    LINK_LOGOUT=(By.XPATH,"//*[@id='app']/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a")
+    LOGOUT_BUTTON = (By.XPATH, "//span[contains(@class, 'MuiButton-label') and text()='Log Out']")
+
     
