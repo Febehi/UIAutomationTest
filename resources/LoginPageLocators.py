ee61e7b0e812a997ea17cab063ddab7a407c6013
Febehi <febehi@integrationobjects.com>
2025-08-01 11:17:39 +0100

    Update LoginPageLocators.py

--- resources/LoginPageLocators.py
+++ resources/LoginPageLocators.py
@@ -6,13 +6,4 @@ class LoginPageLocators:
     PASSWORD_INPUT = (By.NAME, "password")
     LOGIN_BUTTON = (By.XPATH, "//span[contains(@class, 'MuiButton-label')]")
     OK_BUTTON = (By.CSS_SELECTOR, "button.swal-button.swal-button--confirm")
-    ALERTFAILURE=(By.CLASS_NAME,'oxd-alert-content-text')
-    VISIBLE = (By.CLASS_NAME, 'oxd-text--h6')
-    REQUIRED=(By.CLASS_NAME, 'oxd-input-field-error-message')
-    FORGOTPASS=(By.CLASS_NAME, 'orangehrm-login-forgot-header')
-    CANCELBTN= (By.XPATH, "//button[@type='button']")
-    FORGOT_PAGE_TITLE=(By.XPATH, '//*[@id="app"]/div[1]/div[1]/div/form/h6')
-    RESETBTN=(By.XPATH, "//button[@type='submit']")
-    USERNAME_FORGOT_PASS=(By.CLASS_NAME,"oxd-input--active")
-    SEND_PASS_TITLE=(By.CLASS_NAME,"orangehrm-forgot-password-title")
 
