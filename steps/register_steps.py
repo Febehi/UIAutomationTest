78d8ed9bd247d346b07ee98cc1c5d2bcec965b03
Febehi <febehi@integrationobjects.com>
2025-08-01 11:16:46 +0100

    Update register_steps.py

--- steps/register_steps.py
+++ steps/register_steps.py
@@ -8,28 +8,66 @@ def save_screenshot(context, name):
     allure.attach(context.driver.get_screenshot_as_png(), name=name, attachment_type=allure.attachment_type.PNG)
 
 
-@given(u'I am on the register page')
+@given("I am on the register page")
 def step_impl(context):
     context.register_page = RegisterPage(context.driver)
-    context.register_page.visit()
+    assert context.register_page.is_displayed()
     save_screenshot(context, "register_page.png")
 
-@when(u'I click on the Register button')
+
+@when('I enter my username "{username}"')
+def step_impl(context, username):
+    context.register_page.enter_username(username)
+    save_screenshot(context, "entered_username.png")
+
+
+@when('I enter my password "{password}"')
+def step_impl(context, password):
+    context.register_page.enter_password(password)
+    save_screenshot(context, "entered_password.png")
+
+
+@when('I enter my confirm password "{confirm_password}"')
+def step_impl(context, confirm_password):
+    context.register_page.enter_confirm_password(confirm_password)
+    save_screenshot(context, "entered_confirm_password.png")
+
+
+@when("I click on the Register button")
 def step_impl(context):
-    try:
-        context.register_page.click_register_button()
-        logging.info("Clicked the Register button")
-        save_screenshot(context, "Register_button_clicked.png")
-    except Exception as e:
-        logging.error("An error occurred while clicking the Register button: %s", str(e))
+    context.register_page.click_register_button()
+    logging.info("Clicked on Register button")
+    save_screenshot(context, "register_button_clicked.png")
 
 
-@then(u'I should receive a success alert "{msgalert}"')
-def step_impl(context, msgalert):
-    msgalert = context.register_page.get_success_message()
-    save_screenshot(context, "SuccessAlert_received.png") \
+@then('I should receive a success alert "{alert}"')
+def step_impl(context, alert):
+    actual_alert = context.register_page.get_success_alert()
+    assert actual_alert == alert, f"Expected: '{alert}', Got: '{actual_alert}'"
+    logging.info("Success alert is shown")
+    save_screenshot(context, "success_alert.png")
 
-@then(u'I will be redirect to the login page')
+
+@then('I should receive an error alert "{alert}"')
+def step_impl(context, alert):
+    actual_alert = context.register_page.get_error_alert()
+    assert actual_alert == alert, f"Expected: '{alert}', Got: '{actual_alert}'"
+    logging.info("Error alert is shown")
+    save_screenshot(context, "error_alert.png")
+
+
+
+@then("Click on the OK button")
 def step_impl(context):
-    context.login_page.assert_login_displayed()
-    save_screenshot(context, "Redirect_Login.png")
+    context.register_page.click_ok_button()
+    logging.info("Clicked OK on alert")
+    save_screenshot(context, "clicked_ok_alert.png")
+
+
+@then("I will be redirect to the login page")
+def step_impl(context):
+    from pages.login_page import LoginPage
+    context.login_page = LoginPage(context.driver)
+    assert context.login_page.is_displayed()
+    logging.info("Successfully redirected to login page")
+    save_screenshot(context, "redirected_to_login.png")
